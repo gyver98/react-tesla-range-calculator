61cf29d0ce0b471a552b65897428c57a6e2b37b1
gyver98 <matthewchoi7@gmail.com>
2017-02-09 10:36:33 +1100

    update className expression

--- src/components/TeslaWheels.js
+++ src/components/TeslaWheels.js
@@ -6,7 +6,7 @@ const TeslaWheels = (props) => (
   <div className="tesla-wheels__component">
     <p className="tesla-wheels__title">Wheels</p>
     <div className="tesla-wheels__container cf">
-      <label className="tesla-wheels__item tesla-wheels__item--{size}">
+      <label className={`tesla-wheels__item tesla-wheels__item--${size}`}>
         <input
           type="radio"
           name="wheelsize"
