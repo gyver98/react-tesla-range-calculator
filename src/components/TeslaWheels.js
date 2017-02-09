8e008220b911e01cb252984bbcb12c0af00d442b
gyver98 <matthewchoi7@gmail.com>
2017-02-09 11:01:41 +1100

    adding LabelLists in TeslaWheels comp

--- src/components/TeslaWheels.js
+++ src/components/TeslaWheels.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import './TeslaWheels.css';
 
-const size = 19;
+function LabelLists() {
+  const sizes = [19, 21];
+  const LabelItems = sizes.map(size => (
+    <label className={`tesla-wheels__item tesla-wheels__item--${size}`}>
+      <input
+        type="radio"
+        name="wheelsize"
+        value="size"
+        checked="value === size" />
+      <p>
+        {size}
+      </p>
+    </label> 
+    )
+  );
+  return (
+    <div>
+      {LabelItems}
+    </div>
+  );
+}
+
 const TeslaWheels = (props) => (
   <div className="tesla-wheels__component">
     <p className="tesla-wheels__title">Wheels</p>
     <div className="tesla-wheels__container cf">
-      <label className={`tesla-wheels__item tesla-wheels__item--${size}`}>
-        <input
-          type="radio"
-          name="wheelsize"
-          value="size"
-          checked="value === size" />
-        <p>
-          {size}
-        </p>
-      </label>
+      <LabelLists />
     </div>
   </div>
 );
