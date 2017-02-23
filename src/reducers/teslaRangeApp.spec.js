b573aea27f3dfe2db46223d9c6e8dc18c18a919e
gyver98 <matthewchoi7@gmail.com>
2017-02-23 13:44:49 +1100

    test cases

--- src/reducers/teslaRangeApp.spec.js
+++ src/reducers/teslaRangeApp.spec.js
@@ -21,23 +21,6 @@ const initialState = {
 
 const climateChangeState = {
   carstats:[
-    {miles:246, model:"60"},
-    {miles:250, model:"60D"},
-    {miles:297, model:"75"},
-    {miles:306, model:"75D"},
-    {miles:336, model:"90D"},
-    {miles:376, model:"P100D"}
-  ],
-  config: {
-    speed: 55,
-    temperature: 20,
-    climate: false,
-    wheels: 19
-  }
-}
-
-const updatedStatsAfterClimateChange = {
-  carstats:[
     {miles:267, model:"60"},
     {miles:273, model:"60D"},
     {miles:323, model:"75"},
@@ -54,23 +37,6 @@ const updatedStatsAfterClimateChange = {
 }
 
 const speedUpState = {
-  carstats:[
-    {miles:267, model:"60"},
-    {miles:273, model:"60D"},
-    {miles:323, model:"75"},
-    {miles:334, model:"75D"},
-    {miles:366, model:"90D"},
-    {miles:409, model:"P100D"}
-  ],
-  config: {
-    speed: 60,
-    temperature: 20,
-    climate: false,
-    wheels: 19
-  }
-}
-
-const updatedStatsAfterSpeedUp = {
   carstats:[
     {miles:242, model:"60"},
     {miles:248, model:"60D"},
@@ -89,12 +55,12 @@ const updatedStatsAfterSpeedUp = {
 
 const speedDownState = {
   carstats:[
-    {miles:242, model:"60"},
-    {miles:248, model:"60D"},
-    {miles:292, model:"75"},
-    {miles:303, model:"75D"},
-    {miles:332, model:"90D"},
-    {miles:371, model:"P100D"}
+    {miles:267, model:"60"},
+    {miles:273, model:"60D"},
+    {miles:323, model:"75"},
+    {miles:334, model:"75D"},
+    {miles:366, model:"90D"},
+    {miles:409, model:"P100D"}
   ],
   config: {
     speed: 55,
@@ -105,23 +71,6 @@ const speedDownState = {
 }
 
 const wheelChangeState = {
-  carstats:[
-    {miles:246, model:"60"},
-    {miles:250, model:"60D"},
-    {miles:297, model:"75"},
-    {miles:306, model:"75D"},
-    {miles:336, model:"90D"},
-    {miles:376, model:"P100D"}
-  ],
-  config: {
-    speed: 55,
-    temperature: 20,
-    climate: true,
-    wheels: 21
-  }
-}
-
-const updateStatAfterChangeWheel = {
   carstats:[
     {miles:241, model:"60"},
     {miles:246, model:"60D"},
@@ -138,6 +87,7 @@ const updateStatAfterChangeWheel = {
   }
 }
 
+
 describe('test reducer', () => {
   it('should handle initial stat', () => {
     expect(
@@ -152,68 +102,37 @@ describe('test reducer', () => {
       })
     ).toEqual(climateChangeState)
   })
+  
+  // it('should handle SPEED_UP', () => {
+  //   expect(
+  //     updateConfig(climateChangeState,{
+  //       type: 'SPEED_UP',
+  //       value: 55,
+  //       step: 5,
+  //       maxValue: 70
+  //     })
+  //   ).toEqual(speedUpState)
+  // })
+  
+  // it('should handle SPEED_DOWN', () => {
+  //   expect(
+  //     updateConfig(speedUpState,{
+  //       type: 'SPEED_DOWN',
+  //       value: 60,
+  //       step: 5,
+  //       maxValue: 70
+  //     })
+  //   ).toEqual(speedDownState)
+  // })
+  
+  // it('should handle CHANGE_WHEEL', () => {
+  //   expect(
+  //     updateConfig(initialState,{
+  //       type: 'CHANGE_WHEEL',
+  //       value: 21
+  //     })
+  //   ).toEqual(wheelChangeState)
+  // })
 
-  it('should handle UPDATE_STATS', () => {
-    expect(
-      updateConfig(climateChangeState,{
-        type: 'UPDATE_STATS'
-      })
-    ).toEqual(updatedStatsAfterClimateChange)
-  })
-
-  it('should handle SPEED_UP', () => {
-    expect(
-      updateConfig(updatedStatsAfterClimateChange,{
-        type: 'SPEED_UP',
-        value: 55,
-        step: 5,
-        maxValue: 70
-      })
-    ).toEqual(speedUpState)
-  })
-
-  it('should handle UPDATE_STATS', () => {
-    expect(
-      updateConfig(speedUpState,{
-        type: 'UPDATE_STATS'
-      })
-    ).toEqual(updatedStatsAfterSpeedUp)
-  })
-
-  it('should handle SPEED_DOWN', () => {
-    expect(
-      updateConfig(updatedStatsAfterSpeedUp,{
-        type: 'SPEED_DOWN',
-        value: 60,
-        step: 5,
-        maxValue: 70
-      })
-    ).toEqual(speedDownState)
-  })
-
-  it('should handle UPDATE_STATS', () => {
-    expect(
-      updateConfig(speedDownState,{
-        type: 'UPDATE_STATS'
-      })
-    ).toEqual(updatedStatsAfterClimateChange)
-  })
-
-  it('should handle CHANGE_WHEEL', () => {
-    expect(
-      updateConfig(initialState,{
-        type: 'CHANGE_WHEEL',
-        value: 21
-      })
-    ).toEqual(wheelChangeState)
-  })
-
-  it('should handle UPDATE_STATS', () => {
-    expect(
-      updateConfig(wheelChangeState,{
-        type: 'UPDATE_STATS'
-      })
-    ).toEqual(updateStatAfterChangeWheel)
-  })
 })
 
