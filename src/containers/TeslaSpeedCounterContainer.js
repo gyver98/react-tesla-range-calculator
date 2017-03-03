8f4b5c7710850a734b8861252d424c330c778d01
somdari <matthewchoi7@gmail.com>
2017-03-04 10:25:06 +1100

    comment out help functions in TeslaSpeedCounterContainer

--- src/containers/TeslaSpeedCounterContainer.js
+++ src/containers/TeslaSpeedCounterContainer.js
@@ -3,23 +3,23 @@ import { connect } from 'react-redux';
 import { speedUp, speedDown } from '../actions'
 import { counterDefaultVal } from '../constants/counterDefaultVal';
 
-const isLessThanMax = (currentValue) => {
-  //debugger;
-  const maxValue = counterDefaultVal.speed.max;
-  const step = counterDefaultVal.speed.step;
-  if(currentValue < maxValue) { 
-    return true 
-  }
-}
+// const isLessThanMax = (currentValue) => {
+//   //debugger;
+//   const maxValue = counterDefaultVal.speed.max;
+//   const step = counterDefaultVal.speed.step;
+//   if(currentValue < maxValue) { 
+//     return true 
+//   }
+// }
 
-const isMoreThanMin = (currentValue) => {
-  //debugger;
-  const minValue = counterDefaultVal.speed.min;
-  const step = counterDefaultVal.speed.step;
-  if(currentValue > minValue) { 
-    return true 
-  }
-}
+// const isMoreThanMin = (currentValue) => {
+//   //debugger;
+//   const minValue = counterDefaultVal.speed.min;
+//   const step = counterDefaultVal.speed.step;
+//   if(currentValue > minValue) { 
+//     return true 
+//   }
+// }
 
 
 const mapStateToProps = (state) => {
@@ -33,10 +33,12 @@ const mapDispatchToProps = (dispatch) => {
   //debugger;
   return {
     increment:(value) => {
-      isLessThanMax(value)? dispatch(speedUp(value)) : null
+      //isLessThanMax(value)? dispatch(speedUp(value)) : null
+      dispatch(speedUp(value))
     },
     decrement:(value) => {
-      isMoreThanMin(value)? dispatch(speedDown(value)) : ''
+      //isMoreThanMin(value)? dispatch(speedDown(value)) : ''
+      dispatch(speedDown(value))
     }
   }
 }
