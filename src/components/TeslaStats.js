14b2c118a0effc5ded167e8ac1fa332ce343a87e
gyver98 <matthewchoi7@gmail.com>
2017-03-22 15:57:35 +1100

    delete comments

--- src/components/TeslaStats.js
+++ src/components/TeslaStats.js
@@ -3,7 +3,7 @@ import './TeslaStats.css';
 
 const TeslaStats = (props) => {
   const carstats = props.carstats.map((stat) => (
-    <li key={stat.model} className="stats-animation">
+    <li key={stat.model}>
       <div className={`tesla-stats-icon tesla-stats-icon--${stat.model.toLowerCase()}`}></div>
       <p>{stat.miles}</p>
     </li>
--- src/containers/TeslaSpeedCounterContainer.js
+++ src/containers/TeslaSpeedCounterContainer.js
@@ -3,42 +3,20 @@ import { connect } from 'react-redux';
 import { speedUp, speedDown } from '../actions'
 import { counterDefaultVal } from '../constants/counterDefaultVal';
 
-// const isLessThanMax = (currentValue) => {
-//   //debugger;
-//   const maxValue = counterDefaultVal.speed.max;
-//   const step = counterDefaultVal.speed.step;
-//   if(currentValue < maxValue) { 
-//     return true 
-//   }
-// }
-
-// const isMoreThanMin = (currentValue) => {
-//   //debugger;
-//   const minValue = counterDefaultVal.speed.min;
-//   const step = counterDefaultVal.speed.step;
-//   if(currentValue > minValue) { 
-//     return true 
-//   }
-// }
-
 
 const mapStateToProps = (state) => {
   return {
     currentValue: state.config.speed,
-    initValues: counterDefaultVal.speed,
-    animationEffect: true
+    initValues: counterDefaultVal.speed
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  //debugger;
   return {
     increment:(value) => {
-      //isLessThanMax(value)? dispatch(speedUp(value)) : null
       dispatch(speedUp(value))
     },
     decrement:(value) => {
-      //isMoreThanMin(value)? dispatch(speedDown(value)) : ''
       dispatch(speedDown(value))
     }
   }
